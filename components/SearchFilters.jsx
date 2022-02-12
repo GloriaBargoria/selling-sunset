4d8d754d3c8225cd941b4561f838ccf688224033
GloriaBargoria <bargoriagloria@gmail.com>
2022-02-12 18:52:57 +0300

    search filters implemented

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,12 +1,46 @@
 import { useEffect, useState } from "react";
 import { Box, Button, Flex, Select, Input, Spinner, Icon } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { MdCancel } from 'react-icons/md';
 import Image from "next/image";
 
+import { filterData, getFilterValues } from "../utils/filterData";
+
+const searchProperties = (filterValues) => {
+    // const router = useRouter();
+    const path = Router.pathname;
+    const {query} = Router;
+
+    const values = getFilterValues(filterValues);
+
+    values.forEach((item) => {
+        query[item.name] = item.value
+    })
+
+    Router.push({ pathname: path, query })
+}
+
 const SearchFilters = () => {
+    const [filters, setFilters] =useState(filterData);
     return(
-        <div>Search FIlters</div>
+        <Flex bg="gray.100" p="5" justifyContent="center" flexWrap="wrap">
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                    <Select 
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}
+                    >
+                        {filter?.items?.map((item) => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
     );
 }
 
