179c1ff2b0f8f55e8c554ee223f2829bf4ab68d8
GloriaBargoria <bargoriagloria@gmail.com>
2022-02-18 19:57:49 +0300

    changes on fetchApi

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -8,7 +8,7 @@ export const fetchApi = async (url) => {
     const {data} = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '6300af3bc6mshab5b7801a1dbd0cp1309b7jsna533ff1bd3a5'
+            'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
         }
     });
 
